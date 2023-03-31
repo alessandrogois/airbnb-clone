ba6ce6ef0b4e016f56d88c9d55bf300144962c3e
Alessandro Gois <alfe.gois@gmail.com>
2023-03-31 14:54:37 -0300

    First commit create envrioment to project

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () =>{
+  return (<div className="fixed w-full bg-white z-10 shadow-sm" > 
+    <div className="py-4 border-b-[1px]">
+      <Continer></Continer>
+    </div>
+   </div>);
+}
+
+export default Navbar;
